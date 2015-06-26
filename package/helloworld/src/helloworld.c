aa712a9552d76b2531a6c422cb8e39528f3ccaba
cuiqingwei <cuiqingwei@gmail.com>
2015-06-26 16:54:29 +0800

    hello world

---
+++ package/helloworld/src/helloworld.c
@@ -0,0 +1,14 @@
+/**************** 
+* Helloworld.c 
+*****************/
+
+#include <stdio.h> 
+#include <unistd.h> 
+
+int main(void) 
+{ 
+	printf("Hell! O' world, why won't my code compile?\n\n"); 
+	return 0; 
+}
+
+
